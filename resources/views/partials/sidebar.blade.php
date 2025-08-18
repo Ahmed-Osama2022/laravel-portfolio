6871e531526c2794e78dbb8296787cef43650036
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-08-18 21:40:56 +0300

    Finish the sidebar for mobile phones. (NavBar).

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,5 @@
-<div class="sidebar pe-2">
+{{-- For >= Desktop screens --}}
+<div class="sidebar pe-2 d-none d-lg-block">
   <div class="items">
     <ul class="d-flex gap-4 flex-column p-4 justify-content-center align-items-center rounded-pill shadow">
       <li data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Home" class="active"><i
@@ -13,6 +14,20 @@
            class="fa-solid fa-envelope fs-4"></i></li>
     </ul>
 
+  </div>
+</div>
+
+{{-- For < Desktop screens (Mobile & Tablets) --}}
+<div class="sidebar-phone d-block d-lg-none">
+  <div class="items">
+    <ul class="d-flex gap-3 flex-row p-4 my-4 justify-content-around align-items-center rounded shadow">
+      <li class="active"><i class="fa-solid fa-house fs-4"></i></li>
+      <li><i class="fa-solid fa-user fs-4"></i>
+      </li>
+      <li><i class="fa-solid fa-screwdriver-wrench fs-4"></i></li>
+      <li><i class="fa-solid fa-laptop-code fs-4"></i></li>
+      <li><i class="fa-solid fa-envelope fs-4"></i></li>
+    </ul>
 
   </div>
 </div>
