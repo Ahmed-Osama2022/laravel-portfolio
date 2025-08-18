4685b06b401374003f0ee4adbfbb9a9c21ce6dd7
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-08-18 23:12:33 +0300

    Make the (Top) navbar appear elegant in the mobile screens and finish it.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,27 +1,33 @@
   <nav class="navbar navbar-expand-lg bg-transparent py-4">
     <div class="container">
       {{-- <a class="navbar-brand" href="#">{{ env('APP_NAME') }}</a> --}}
-      <div class="d-flex flex-row align-items-center justify-content-between w-100">
+
+      {{-- LOGO --}}
+      <div
+           class="d-flex flex-column flex-md-row text-center text-md-start align-items-center justify-content-between w-100">
         <div class="">
           <a class="navbar-brand p-0" href="#">
             <img src="{{ asset('assets/images/LOGO(Ahmed)-sm.svg') }}" class="img-fluid p-0" alt="LOGO Image">
           </a>
         </div>
-        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button> --}}
 
+        {{-- Social media icons & links --}}
         <div class="" id="navbarNav">
-          <ul class="d-flex flex-row  align-items-center justify-content-between gap-3 m-auto">
+          <ul class="d-flex flex-row align-items-center justify-content-between gap-3 m-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#"><i class="fa-brands fa-facebook fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#"><i class="fa-brands fa-x-twitter fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="#"><i class="fa-brands fa-instagram fs-5"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-brands fa-linkedin fs-5"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-brands fa-github fs-5"></i></a>
             </li>
           </ul>
         </div>
