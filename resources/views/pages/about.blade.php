c01f8b5d0b751b530d2f8bd655c0656d96d01a8b
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-08-20 01:58:31 +0300

    Modify the responsie for the page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="container">
-    <div class="d-flex bg-danger align-items-center p-3 text-center text-md-start">
+    <div class="d-flex bg-danger align-items-center p-3 justify-content-center  justify-content-md-start">
       <h3>About Page</h3>
     </div>
   </div>
