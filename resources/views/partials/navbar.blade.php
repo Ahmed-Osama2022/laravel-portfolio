e37208b60d0b98e576dac199339b100fc921a850
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-08-28 22:07:40 +0300

    Finish the social links to be dynamic in the top navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
   <nav class="navbar navbar-expand-lg bg-transparent py-4" data-aos="fade-down">
     <div class="container">
-      {{-- <a class="navbar-brand" href="#">{{ env('APP_NAME') }}</a> --}}
+      {{-- <a class="navbar-brand" href="#1">{{ env('APP_NAME') }}</a> --}}
 
       {{-- LOGO --}}
       <div
@@ -13,24 +13,33 @@
 
         {{-- Social media icons & links --}}
         <div class="" id="navbarNav">
+          @php
+            $social_links = App\Models\Social::find(1) ?? '#';
+          @endphp
           <ul class="d-flex flex-row align-items-center justify-content-between gap-3 m-auto px-0">
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-facebook fs-5"></i></a>
+              <a class="nav-link" href="{{ $social_links->facebook ?? '#' }}"><i
+                   class="fa-brands fa-facebook fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-x-twitter fs-5"></i></a>
+              <a class="nav-link" href="{{ $social_links->X_twitter ?? '#' }}"><i
+                   class="fa-brands fa-x-twitter fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-instagram fs-5"></i></a>
+              <a class="nav-link" href="{{ $social_links->instagram ?? '#' }}"><i
+                   class="fa-brands fa-instagram fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-linkedin fs-5"></i></a>
+              <a class="nav-link" href="{{ $social_links->linkedin ?? '#' }}"><i
+                   class="fa-brands fa-linkedin fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-github fs-5"></i></a>
+              <a class="nav-link" href="{{ $social_links->github ?? '#' }}"><i class="fa-brands fa-github fs-5"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><i class="fa-brands fa-whatsapp fs-5"></i></a>
+              <a class="nav-link"
+                 href="{{ $social_links->whatsapp ? 'https://wa.me/+2' . $social_links->whatsapp : '#' }}"><i
+                   class="fa-brands fa-whatsapp fs-5"></i></a>
             </li>
             @if (Auth::check())
               <li class="">
